93d6878e4bc7b5f15b2e5364ad50cae1a9746c5a
Peter Richards <pejrich@gmail.com>
2024-11-09 06:15:58 -0800

    fixed python_path not accepting list of strings

--- lib/export/helpers.ex
+++ lib/export/helpers.ex
@@ -2,16 +2,24 @@ defmodule Export.Helpers do
   @moduledoc false
 
   def convert_options(options) do
-    options |> Enum.map(fn (opt)->
-      case opt do
-        {key, other_options} when other_options |> is_list ->
-          {key, other_options |> Enum.map(fn ({key, value}) ->
-            {key |> Atom.to_charlist, value |> String.to_charlist}
-          end)}
-        {key, value} when value |> is_bitstring ->
-          {key, value |> String.to_charlist}
-        _ -> opt
-      end
+    options
+    |> Enum.map(fn
+      {key, ["" <> _ | _] = list} ->
+        {key, Enum.map(list, &to_charlist/1)}
+
+      {key, other_options} when is_list(other_options) ->
+        {key,
+         other_options
+         |> Enum.map(fn
+           {key, value} ->
+             {key |> Atom.to_charlist(), value |> String.to_charlist()}
+         end)}
+
+      {key, "" <> value} ->
+        {key, value |> String.to_charlist()}
+
+      opt ->
+        opt
     end)
   end
 end
